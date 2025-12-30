23744d10d7ad8327b3bc22c02f4e1fc7c09d8fde
HMD-A <hamada-sota-jp@ynu.jp>
2025-12-30 09:01:04 +0900

    なんでこんなchangeあるの？

--- lib/hello-cdk-stack.ts
+++ lib/hello-cdk-stack.ts
@@ -4,19 +4,20 @@ import { aws_lambda, Stack, StackProps } from 'aws-cdk-lib';
 import { PythonFunction } from '@aws-cdk/aws-lambda-python-alpha';
 // import * as sqs from 'aws-cdk-lib/aws-sqs';
 import * as lambda from 'aws-cdk-lib/aws-lambda';
+import * as path from 'path';
 
 export class HelloCdkStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
       // Define the Lambda function resource
-    const myFunction = new PythonFunction(this, "HelloWorldFunction",{        
-      runtime: aws_lambda.Runtime.PYTHON_3_11,
-      entry: 'src/lambda/hello',
-      handler: 'handler',
-      bundling: {
-        assetExcludes: ['.env', '.venv', 'venv', '__pycache__', '*.pyc'],
-      },
+    const myFunction = new lambda.DockerImageFunction(this, 'AssetFunction', {
+      code: lambda.DockerImageCode.fromImageAsset(path.join(__dirname, '../src'), {
+        cmd: ["index.handler"],
+      }),
+      architecture: lambda.Architecture.ARM_64,
+      memorySize: 1024,
+      timeout: cdk.Duration.seconds(30),
     });
 
     // Define the Lambda function URL resource
